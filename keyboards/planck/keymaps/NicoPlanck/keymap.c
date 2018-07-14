ff2b443f7c600a1a2e67909a0ca674aaac9f4cd2
Nicol <31802947+Kotus91@users.noreply.github.com>
2018-07-14 17:28:36 +0200

    Inversion DEL et Enter pour Enter sur rangée de repos == + pratique

--- keyboards/planck/keymaps/NicoPlanck/keymap.c
+++ keyboards/planck/keymaps/NicoPlanck/keymap.c
@@ -45,17 +45,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 * ,-----------------------------------------------------------------------------------.
 * | Esc  |   A  |   Z  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
 * |------+------+------+------+------+-------------+------+------+------+------+------|
-* | Tab  |   Q  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   M  | Del  |
+* | Tab  |   Q  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   M  |Enter |
 * |------+------+------+------+------+------|------+------+------+------+------+------|
-* | Shift|   W  |   X  |   C  |   V  |   B  |   N  |   ,  |   .  |   :  |   !  |Enter |
+* | Shift|   W  |   X  |   C  |   V  |   B  |   N  |   ,  |   .  |   :  |   !  | Del  |
 * |------+------+------+------+------+------+------+------+------+------+------+------|
 * | Ctrl | Menu | Alt  | GUI  |Lower |    Space    |Raise | Left |  Up  | Down |Right |
 * `-----------------------------------------------------------------------------------'
 */
 [_AZERTY] = {
     {KC_ESC,  KC_A,    KC_Z,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
-    {KC_TAB,  KC_Q,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_DEL},
-    {KC_LSFT, KC_W,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_COMM, KC_DOT,  KC_DOT,  KC_SLSH, KC_ENT },
+    {KC_TAB,  KC_Q,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT},
+    {KC_LSFT, KC_W,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_COMM, KC_DOT,  KC_DOT,  KC_SLSH, KC_DEL },
     {KC_LCTL, KC_APP,  KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT}
   },
 
@@ -81,17 +81,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |   B  |   É  |   P  |   O  |   È  |   ^  |   V  |   D  |   L  |   J  |   Z  | Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |   A  |   U  |   I  |   E  |   ,  |   C  |   T  |   S  |   R  |   N  |   M  |  Del |
+ * |   A  |   U  |   I  |   E  |   ,  |   C  |   T  |   S  |   R  |   N  |   M  |Enter |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift|   Y  |   X  |   .  |   K  |   '  |   Q  |   G  |   H  |   F  |   W  |Enter |
+ * | Shift|   Y  |   X  |   .  |   K  |   '  |   Q  |   G  |   H  |   F  |   W  |  Del |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Ctrl | Menu | Alt  | GUI  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
  */
 [_BEPO] = {
   {BP_B,    BP_ECUT, KC_P,    KC_O,    BP_EGRV , BP_DCRC , KC_V,    KC_D,  KC_L,    KC_J,    KC_Z,  KC_BSPC},
-  {BP_A,    KC_U,    KC_I,    KC_E,    BP_COMM,  KC_C,     KC_T,    KC_S,  KC_R,    KC_N,    KC_M,  KC_DEL},
-  {KC_LSFT, KC_Y,    KC_X,    KC_DOT,  KC_K,     BP_APOS,  KC_Q,    KC_G,  KC_H,    KC_F,    KC_W,  KC_ENT },
+  {BP_A,    KC_U,    KC_I,    KC_E,    BP_COMM,  KC_C,     KC_T,    KC_S,  KC_R,    KC_N,    KC_M,  KC_ENT},
+  {KC_LSFT, KC_Y,    KC_X,    KC_DOT,  KC_K,     BP_APOS,  KC_Q,    KC_G,  KC_H,    KC_F,    KC_W,  KC_DEL },
   {KC_LCTL, KC_APP,  KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT}
 },
 
@@ -99,17 +99,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
 * |  Esc  |   !  |   ?  |  :   |      |      |      |   7  |   8  |   9  |   +  | Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Tab  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   4  |   5  |   6  |   -  | Del  |
+ * | Tab  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   4  |   5  |   6  |   -  |Enter |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift|  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |   1  |   2  |  3   |   *  |Enter |
+ * | Shift|  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |   1  |   2  |  3   |   *  | Del  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Ctrl |      |      |      |      |             |      |   0  |   .  | ISO /|  =   |
  * `-----------------------------------------------------------------------------------'r
  */
 [_LOWER] = {
   {KC_ESC,  _______, _______, _______, _______, _______, _______, KC_7,    KC_8,  KC_9,   KC_PLUS, KC_BSPC},
-  {KC_TAB,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_4,    KC_5,  KC_6,   KC_MINS, KC_DEL},
-  {KC_LSFT, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_1,    KC_2,  KC_3,   KC_ASTR, KC_ENT},
+  {KC_TAB,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_4,    KC_5,  KC_6,   KC_MINS, KC_ENT},
+  {KC_LSFT, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_1,    KC_2,  KC_3,   KC_ASTR, KC_DEL},
   {KC_LCTL, _______, _______, _______, _______, _______, _______, _______, KC_0,  KC_DOT, KC_SLSH, KC_EQL}
 },
 
