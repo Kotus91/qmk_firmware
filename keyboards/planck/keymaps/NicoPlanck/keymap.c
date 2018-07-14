cb8cfccdc2f4db8aa509778ee284241a3a920160
Nicol <31802947+Kotus91@users.noreply.github.com>
2018-07-14 17:19:46 +0200

    Correction bugs ==> compilation OK

--- keyboards/planck/keymaps/NicoPlanck/keymap.c
+++ keyboards/planck/keymaps/NicoPlanck/keymap.c
@@ -74,7 +74,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
   {KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
   {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT },
-  {BACKLIT, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
+  {KC_LCTL, KC_APP,  KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT}
 },
 
 /* BEPO
@@ -110,7 +110,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_ESC,  _______, _______, _______, _______, _______, _______, KC_7,    KC_8,  KC_9,   KC_PLUS, KC_BSPC},
   {KC_TAB,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_4,    KC_5,  KC_6,   KC_MINS, KC_DEL},
   {KC_LSFT, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_1,    KC_2,  KC_3,   KC_ASTR, KC_ENT},
-  {KC_LCTL, _______, _______, _______, _______, _______, _______, _______, KC_0,  KC_DOT, KC_SLSH, KC_EQUL}
+  {KC_LCTL, _______, _______, _______, _______, _______, _______, _______, KC_0,  KC_DOT, KC_SLSH, KC_EQL}
 },
 
 /* Raise
@@ -231,8 +231,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       return false;
       break;
-  }
 */
+  }
   return true;
 }
 
